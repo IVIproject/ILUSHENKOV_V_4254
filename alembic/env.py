27786ceb60024e08f5f42b60cf5820f6d5fae298
IVI <ivi.22official@gmail.com>
2026-04-20 01:03:18 +0300

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.db import Base
 from app.settings import settings
-from app import models  # noqa: F401  # Ensure model metadata is imported
+from app import models
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.database_url)
